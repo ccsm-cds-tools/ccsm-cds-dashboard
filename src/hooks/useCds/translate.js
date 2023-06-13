f3a9b6297dc39973f07df0630f3d4fbc1ffe58e0
Yunwei Wang <>
2023-06-13 14:45:52 -0500

    Add HPV

--- src/hooks/useCds/translate.js
+++ src/hooks/useCds/translate.js
@@ -1,87 +1,107 @@
-const customCytologyCodes = [
+const testCodeMapping = [
   {
-    text: "Negative for intraepithelial lesion or malignancy, atrophic pattern",
-    code: "373887005"
+    test_code: ['47527-7'],
+    test_name: 'Cytology',
+    map: [
+      {
+        text: "Negative for intraepithelial lesion or malignancy, atrophic pattern",
+        code: "373887005"
+      },
+      {
+        text: "Atypical squamous cells of undetermined significance",
+        code: "441087007"
+      },
+      {
+        text: "Atypical squamous cells of undetermined significance, cannot exclude high grade",
+        code: "441088002"
+      },
+      {
+        text: "Low grade squamous intraepithelial lesion",
+        code: "62051000119105"
+      },
+      {
+        text: "High grade squamous intraepithelial lesion",
+        code: "62061000119107"
+      },
+      {
+        text: "High grade squamous intraepithelial lesion, moderate dysplasia",
+        code: "62061000119107"
+      },
+      {
+        text: "High grade squamous intraepithelial lesion, severe dysplasia",
+        code: "62061000119107"
+      },
+      {
+        text: "High grade squamous intraepithelial lesion, Carcinoma in situ dysplasia",
+        code: "62061000119107"
+      },
+      {
+        text: "invasive squamous cell carcinoma",
+        code: "416033009"
+      },
+      {
+        text: "Atypical glandular cells, NOS",
+        code: "441219009"
+      },
+      {
+        text: "Atypical endocervial cells, NOS",
+        code: "448406002"
+      },
+      {
+        text: "Atypical endometrial cells, NOS",
+        code: "441219009"
+      },
+      {
+        text: "Endocervical adenocarcinoma in situ",
+        code: "254890008"
+      },
+      {
+        text: "Adenocarcinoma, not otherwise specified",
+        code: "254890008"
+      },
+      {
+        text: "invasive endocervical adenocarcinoma",
+        code: "254890008"
+      },
+      {
+        text: "Endometrial adenocarcinoma",
+        code: "123845008"
+      },
+      {
+        text: "Other malignant neoplasm, see comment",
+        code: "416033009"
+      },
+      {
+        text: "High grade squamous intraepithelial lesion, cannot rule out invasion",
+        code: "373879009"
+      },
+      {
+        text: "Unable to interpret",
+        code: "126481000119106"
+      },
+      {
+        text: "Malignant cells present",
+        code: "416033009"
+      },
+      {
+        text: "Reparative/reactive changes",
+        code: "373887005"
+      }
+    ]
   },
   {
-    text: "Atypical squamous cells of undetermined significance",
-    code: "441087007"
-  },
-  {
-    text: "Atypical squamous cells of undetermined significance, cannot exclude high grade",
-    code: "441088002"
-  },
-  {
-    text: "Low grade squamous intraepithelial lesion",
-    code: "62051000119105"
-  },
-  {
-    text: "High grade squamous intraepithelial lesion",
-    code: "62061000119107"
-  },
-  {
-    text: "High grade squamous intraepithelial lesion, moderate dysplasia",
-    code: "62061000119107"
-  },
-  {
-    text: "High grade squamous intraepithelial lesion, severe dysplasia",
-    code: "62061000119107"
-  },
-  {
-    text: "High grade squamous intraepithelial lesion, Carcinoma in situ dysplasia",
-    code: "62061000119107"
-  },
-  {
-    text: "invasive squamous cell carcinoma",
-    code: "416033009"
-  },
-  {
-    text: "Atypical glandular cells, NOS",
-    code: "441219009"
-  },
-  {
-    text: "Atypical endocervial cells, NOS",
-    code: "448406002"
-  },
-  {
-    text: "Atypical endometrial cells, NOS",
-    code: "441219009"
-  },
-  {
-    text: "Endocervical adenocarcinoma in situ",
-    code: "254890008"
-  },
-  {
-    text: "Adenocarcinoma, not otherwise specified",
-    code: "254890008"
-  },
-  {
-    text: "invasive endocervical adenocarcinoma",
-    code: "254890008"
-  },
-  {
-    text: "Endometrial adenocarcinoma",
-    code: "123845008"
-  },
-  {
-    text: "Other malignant neoplasm, see comment",
-    code: "416033009"
-  },
-  {
-    text: "High grade squamous intraepithelial lesion, cannot rule out invasion",
-    code: "373879009"
-  },
-  {
-    text: "Unable to interpret",
-    code: "126481000119106"
-  },
-  {
-    text: "Malignant cells present",
-    code: "416033009"
-  },
-  {
-    text: "Reparative/reactive changes",
-    code: "373887005"
+    test_code: ['71431-1', '77399-4', '77400-0'],
+    test_name: 'HPV',
+    map: [
+      {
+        text: "Not detected, negative",
+        code: "260415000"
+      },
+      {
+        test: "Detected, postive",
+        code: "260373001"
+      }
+    ]
   }
 ]
 
@@ -95,12 +115,14 @@ const SCT_URL = 'http://snomed.info/sct'
  */
 export function translateResponse(patientData) {
   patientData
-    .filter(pd => pd.resourceType === 'Observation' && pd.code.coding.some(coding => coding.code === '47527-7'))
-    .forEach(pd => mapResult(pd, customCytologyCodes));
+    .filter(pd => pd.resourceType === 'Observation')
+    .forEach(pd => mapResult(pd, testCodeMapping));
 }
 
-function mapResult(result, customCodes) {
-  if (!result.valueCodeableConcept && result.valueString) {
+function mapResult(result, testCodeMapping) {
+  const customCodes = testCodeMapping.find(ts => result.code.coding.some(coding => ts.test_code.includes(coding.code)));
+
+  if (customCodes && !result.valueCodeableConcept && result.valueString) {
     const targetCode = customCodes.find(cc => cc.text === result.valueString);
 
     if (targetCode) {
@@ -114,4 +136,3 @@ function mapResult(result, customCodes) {
     }
   }
 }
-

5eb6f79584205ddf2ff9db9c9c2b4ac0af1f5442
Yunwei Wang <>
2023-06-27 11:02:41 -0500

    fix merge issue

--- src/smart/SmartPatient.js
+++ src/smart/SmartPatient.js
@@ -152,16 +152,6 @@ async function boundParser(data) {
     convert = (c) => module.runGoFSH([JSON.stringify(c)], options);
   }
 
-async function boundParser(data) {
-  const options = { dependencies: [], indent: true };
-
-  let convert = (c) => Promise.resolve(c);
-
-  if (process.env.REACT_APP_DEBUG_FHIR === 'true') {
-    const module = await import('./FSHHelpers');
-    convert = (c) => module.runGoFSH([JSON.stringify(c)], options);
-  }
-
   return async function parseFhir(rsrc) {
     if (!rsrc) return;
 

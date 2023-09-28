0c6f44429ad042f1468528b77788d2d7a3c1b16b
Yunwei Wang <>
2023-09-28 10:51:25 -0500

    Add group result by UNIVESRAL_PAT_ID

--- src/hooks/useCds/convert-strides.mjs
+++ src/hooks/useCds/convert-strides.mjs
@@ -2,7 +2,19 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 import csvToJson from 'convert-csv-to-json';
 
+let keyColumn = 'UNIVESRAL_PAT_ID';
+let groupedResults = {};
 
 const jsonFile = join('src/hooks/useCds/strides.js');
 const results = csvToJson.fieldDelimiter(',').supportQuotedField(true).getJsonFromCsv('data/strides.csv');
-writeFileSync(jsonFile, 'export const stridesDb = ' + JSON.stringify(results, null, 2));
+results.forEach(row => {
+  let key = row[keyColumn]
+
+  if (!groupedResults[key]) {
+    groupedResults[key] = [];
+  }
+
+  groupedResults[key].push(row);
+});
+
+writeFileSync(jsonFile, 'export const stridesDb = ' + JSON.stringify(groupedResults, null, 2));

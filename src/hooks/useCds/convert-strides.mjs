06456c913d78ab93557c265deaeb3d0775955c0d
Yunwei Wang <>
2023-12-05 12:55:31 -0600

    Add Readme instruction and handle file not exist error

--- src/hooks/useCds/convert-strides.mjs
+++ src/hooks/useCds/convert-strides.mjs
@@ -1,13 +1,21 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, existsSync } from 'fs';
 import { join } from 'path';
 import csvToJson from 'convert-csv-to-json';
 
 let keyColumn = 'MRN';
 let groupedResults = {};
-let csvFile = 'data/strides.csv';
+let csvFile = '';
 
 if (process.argv.length === 3) {
   csvFile = process.argv[2];
+} else if (process.argv.length < 3) {
+  console.info("Usage: npm run convert-csv [CSV file path]")
+  process.exit(1);
+}
+
+if (!existsSync(csvFile)) {
+  console.error("Error: File " + csvFile + " does not exist.")
+  process.exit(1);
 }
 
 const jsonFile = join('src/hooks/useCds/strides.js');
@@ -23,3 +31,5 @@ results.forEach(row => {
 });
 
 writeFileSync(jsonFile, 'export const stridesData = ' + JSON.stringify(groupedResults, null, 2));
+
+console.log("CSV file provided is coverted to JSON successfully.")

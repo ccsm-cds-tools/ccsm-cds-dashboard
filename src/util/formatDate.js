18e63cff60b3512152cb4aa0b5369fd94222a310
Michael Nosal <mnosal@mitre.org>
2024-11-26 16:04:14 -0500

    handle date object

--- src/util/formatDate.js
+++ src/util/formatDate.js
@@ -6,6 +6,10 @@
 
 export function formatDate(date) {
   const dateFormat = new Intl.DateTimeFormat("en-US",{year:"numeric",month:"2-digit",day:"2-digit",timeZone:"UTC"});
+  if (typeof date === "object")
+  {
+    date = [String(date?.month ?? ''), String(date?.day ?? ''), String(date?.year ?? '')].join('/');
+  }
   return date ? dateFormat.format(new Date(date)) : null;
 }
 
